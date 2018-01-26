1deff26614e2ac04d187034c8b5db85db3df1212
Federico D'Angelo <federicodangelo@gmail.com>
2018-01-26 00:36:22 -0300

    Added raw-assets generation project

---
+++ rawassets/src/com/fangelo/rawassets/RawAssetsBuilder.java
@@ -0,0 +1,49 @@
+package com.fangelo.rawassets;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class RawAssetsBuilder {
+
+	static private final String REAL_ASSETS_PATH = "../../android/assets/";
+
+	public static void main(String[] args) {
+		System.out.println("packaging assets!");
+
+		packUITextures();
+
+		packDefaultTextures();
+	}
+
+	static public void packUITextures() {
+
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+
+		//settings.fast = true;
+		//settings.grid = true;
+		settings.paddingX = 2;
+		settings.paddingY = 2;
+		//settings.useIndexes = false;
+		//settings.maxWidth = settings.maxHeight = 1024;
+		settings.duplicatePadding = true;
+		settings.filterMag = Texture.TextureFilter.Linear;
+		settings.filterMin = Texture.TextureFilter.Linear;
+
+		TexturePacker.process(settings, "ui", REAL_ASSETS_PATH + "ui", "ui");
+	}
+
+	static public void packDefaultTextures() {
+
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+
+		settings.fast = true;
+		settings.grid = true;
+		settings.paddingX = 2;
+		settings.paddingY = 2;
+		settings.useIndexes = false;
+		settings.maxWidth = settings.maxHeight = 1024;
+		settings.duplicatePadding = true;
+
+		TexturePacker.process(settings, "images", REAL_ASSETS_PATH + "images", "images");
+	}
+}
